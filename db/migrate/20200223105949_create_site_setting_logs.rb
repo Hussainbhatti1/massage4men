09fc932185d1f2f4b75f3baadce3d501d168ed0f
Hussain Bhatti <hussain.bhatti@thedevden.co>
2020-03-22 23:08:56 +0500

    Remove extra migration

--- db/migrate/20200223105949_create_site_setting_logs.rb
+++
@@ -1,9 +0,0 @@
-class CreateSiteSettingLogs < ActiveRecord::Migration
-  def change
-    create_table :site_setting_logs do |t|
-      t.text :site_changes
-
-      t.timestamps null: false
-    end
-  end
-end
